cea0a11d9889d286d2fc6f815f54e4ab2852a83e
rahul <rahulsil930@gmail.com>
2026-06-25 00:12:06 +0530

    ready for deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
-}) 
-API.interceptors.request.use(
-    (config)=>{
-        const token = localStorage.getItem("token");
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    }
-);
+  baseURL:
+    import.meta.env.VITE_API_URL || "http://localhost:4200/api",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default API;
